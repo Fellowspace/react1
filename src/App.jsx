7e9ac6dc9643826f60100a97e403f87f0641cd77
Web Developer <bismahzermin9161@gmail.com>
2025-07-10 21:50:02 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
+import Menu from './components/Menu';
 import '@fontsource/roboto';
 const App = () => {
   return (
     <>
 <Navbar />
 <Home/>
+<About/>
+<Menu/>
     </>
   );
 }
