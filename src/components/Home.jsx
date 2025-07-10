1cfb1ba21e80070559e923cd30526e968782c287
zartash-cell <zartashakram94@gmail.com>
2025-07-10 20:52:29 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,27 +2,35 @@ import React from 'react'
 import Background from '../assets/images/bg.jpg'
 import Roundimg from'../assets/rm1.png'
 import '@fontsource/roboto';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const Home = () => {
   return ( 
     <>
     <section className="home" id="home">
-        <div className="home-overlay"></div>
-<div className="home-content">
+       
+<div className="home-content ">
     <h1>
-        FRESH <span className="highlight">FOOD IN THE</span> MORNING
+        FRESH <span className="highlight ">FOOD IN THE</span> MORNING
     </h1>
-    
-   <p> Satisfy your cravings with our mouth-watering, freshly prepared dishes."
+    <p > Satisfy your cravings with our mouth-watering, freshly prepared dishes."
 "Bringing the taste of comfort and quality to every plate.</p>
-</div>
+ <button className="btn bounce">Get Yours Now</button>
+  </div>
 
-{/* button */}
-<div className="image-container">
+
+    <div className="image-container">
     <img src={Roundimg}
         alt="rm"
       className="custom-image"></img>
+    </div>
+
+
+
+{/* button */}
+
     
-</div>
+
     </section>
 
     </>
