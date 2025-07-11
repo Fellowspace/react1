6d036025d7cb5c9e9fb6d2ad7fce59a8e9e5880e
zartash-cell <zartashakram94@gmail.com>
2025-07-11 13:00:46 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 import Background from '../assets/images/bg.jpg'
 import Roundimg from'../assets/rm1.png'
 import '@fontsource/roboto';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
 const Home = () => {
+   useEffect(() => {
+  AOS.init({ duration: 1200 }); // 👈 animation duration in ms
+}, []);
+
   return ( 
     <>
     <section className="home" id="home">
        
 <div className="home-content ">
-    <h1>
+    <h1 data-aos="fade-right"  data-aos-duration="3000"
+  data-aos-delay="500"
+  data-aos-easing="ease-in-out">
         FRESH <span className="highlight ">FOOD IN THE</span> MORNING
     </h1>
-    <p > Satisfy your cravings with our mouth-watering, freshly prepared dishes."
+    <p  data-aos="fade-up"  data-aos-duration="2500"
+  data-aos-delay="800"
+  data-aos-easing="ease-in-out" > Satisfy your cravings with our mouth-watering, freshly prepared dishes."
 "Bringing the taste of comfort and quality to every plate.</p>
- <button className="btn bounce">Get Yours Now</button>
+
+  <a href="#menu" className="butadd bounce">Get Yours Now</a>
+
   </div>
 
 
