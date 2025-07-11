ec552b52388733ba5af167da307ce8be40bd8cdb
Web Developer <bismahzermin9161@gmail.com>
2025-07-11 14:57:22 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,21 @@
-import React, { useEffect } from 'react';
-
-import AOS from 'aos';
-import 'aos/dist/aos.css'
+import React from 'react'
 import Background from '../assets/images/bg.jpg'
 import Roundimg from'../assets/rm1.png'
 import '@fontsource/roboto';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const Home = () => {
-   useEffect(() => {
-  AOS.init({ duration: 1200 }); // 👈 animation duration in ms
-}, []);
-
   return ( 
     <>
     <section className="home" id="home">
        
 <div className="home-content ">
-    <h1 data-aos="fade-right"  data-aos-duration="3000"
-  data-aos-delay="500"
-  data-aos-easing="ease-in-out">
+    <h1>
         FRESH <span className="highlight ">FOOD IN THE</span> MORNING
     </h1>
-    <p  data-aos="fade-up"  data-aos-duration="2500"
-  data-aos-delay="800"
-  data-aos-easing="ease-in-out" > Satisfy your cravings with our mouth-watering, freshly prepared dishes."
+    <p > Satisfy your cravings with our mouth-watering, freshly prepared dishes."
 "Bringing the taste of comfort and quality to every plate.</p>
-
-  <a href="#menu" className="butadd bounce">Get Yours Now</a>
-
+ <button className="btn bounce"><a href="#menu" className="butadd animation: bounce  ">Get Yours Now</a></button>
   </div>
 
 
