806d68f365e5664499a492dae1f53c72a164a522
zartash-cell <zartashakram94@gmail.com>
2025-07-12 21:00:49 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,3 @@
-
-
 import { useEffect } from 'react';
 import Background from '../assets/images/bg.jpg'
 import Roundimg from'../assets/rm1.png'
@@ -14,23 +12,24 @@ const Home = () => {
     <>
     <section className="home" id="home">
        
-<div className="home-content ">
-    <h1 className="glow-text" data-aos="fade-right"
+<div className="home-content  ">
+    <h1   className="glow-text" data-aos="fade-right"
   data-aos-duration="3000"
   data-aos-delay="500"
-  data-aos-easing="ease-in-out">
-        FRESH <span className="highlight ">FOOD IN THE</span> MORNING
+  data-aos-easing="ease-in-out" >
+        FRESH <span className="highlight  ">FOOD IN THE</span> MORNING
     </h1>
-    <p className="glow-text" data-aos="fade-up"
+    <p  className="glow-text" data-aos="fade-up"
   data-aos-duration="2500"
   data-aos-delay="800"
-  data-aos-easing="ease-in-out" > Satisfy your cravings with our mouth-watering, freshly prepared dishes."
-"Bringing the taste of comfort and quality to every plate.</p>
+  data-aos-easing="ease-in-out" > Satisfy your cravings with our mouth-watering, freshly prepared dishes.
+Bringing the taste of comfort and quality to every plate.</p>
  <div className="get-yours-wrapper">
   <button className="get-yours-btn">
     <a href="#menu" className="color">Get Yours Now</a>
   </button>
 </div>
+
   </div>
 
 
